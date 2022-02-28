2b92fc78d0b9794edb612743b0f3b59a70f28f2d
n40r1s <57507629+n40r1s@users.noreply.github.com>
2022-02-28 13:12:24 +0000

    Create Downloadrepo.ps1

---
+++ Downloadrepo.ps1
@@ -0,0 +1,34 @@
+function Downloadrepo
+{ 
+    param( 
+       [Parameter(Mandatory=$True)] 
+       [string] $Name, 
+        
+       [Parameter(Mandatory=$True)] 
+       [string] $Author, 
+        
+       [Parameter(Mandatory=$False)] 
+       [string] $Branch = "master", 
+        
+       [Parameter(Mandatory=$False)] 
+       [string] $Location = "C:\Users\win8\Downloads\Exploit" 
+    ) 
+    
+    # Force to create a zip file 
+    $ZipFile = "$location\$Name.zip" 
+    New-Item $ZipFile -ItemType File -Force
+    $RepositoryZipUrl = "https://api.github.com/repos/$Author/$Name/zipball/$Branch"  
+    # download the zip 
+    Write-Host 'Starting downloading the GitHub Repository'
+    Invoke-RestMethod -Uri $RepositoryZipUrl -OutFile $ZipFile
+    Write-Host 'Download finished'
+
+    #Extract Zip File
+    Write-Host 'Starting unziping the GitHub Repository locally'
+    Expand-Archive -Path $ZipFile -DestinationPath $location -Force
+    Write-Host 'Unzip finished'
+    
+    # remove zip file
+    Remove-Item -Path $ZipFile -Force 
+}
+[String]$location = Split-Path -Parent $PSCommandPath
